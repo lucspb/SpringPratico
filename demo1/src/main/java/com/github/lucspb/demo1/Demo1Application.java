56669537862ba3ed87ae593bc25782534542fd5c
Lucas Estanislau <lucasestanislau@hotmail.com>
2023-09-02 09:25:42 -0300

    lendo valores do application properties

--- demo1/src/main/java/com/github/lucspb/demo1/Demo1Application.java
+++ demo1/src/main/java/com/github/lucspb/demo1/Demo1Application.java
@@ -1,6 +1,7 @@
 package com.github.lucspb.demo1;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,18 +14,24 @@ public class Demo1Application {
 	@Autowired
 	private AppConfiguration appConfig;
 
+	@Value("${app.message}")
+	private String message;
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(Demo1Application.class, args);
 	}
 
 	@GetMapping("/home")
 	public String inicio(){
-		return "Home";
+		return message;
 	}
 
 	@GetMapping("/configuracao")
 	public String config(){
 		return this.appConfig.getMessage();
 	}
+
+
 
 }
