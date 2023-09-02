96c7b07d05ae093402661cf2c29e374e5a4b653c
Lucas Estanislau <lucasestanislau@hotmail.com>
2023-09-02 10:52:49 -0300

    inicio do proj de cripto e configuracao do banco h2

---
+++ appCrypto/src/test/java/com/lucspb/appCrypto/AppCryptoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucspb.appCrypto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppCryptoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
