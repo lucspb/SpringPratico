99793b5ae1cb53b73c967ec23368960a037b2d48
Lucas Estanislau <lucasestanislau@hotmail.com>
2023-09-04 20:52:24 -0300

    adicionando o getAll

--- appCrypto/src/main/java/com/lucspb/appCrypto/controller/CoinController.java
+++ appCrypto/src/main/java/com/lucspb/appCrypto/controller/CoinController.java
@@ -5,10 +5,7 @@ import com.lucspb.appCrypto.repository.CoinRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.sql.Timestamp;
 
@@ -18,6 +15,11 @@ public class CoinController {
 
     @Autowired
     private CoinRepository coinRepository;
+
+    @GetMapping()
+    public ResponseEntity get(){
+        return new ResponseEntity<>(coinRepository.getAll(), HttpStatus.OK);
+    }
 
     @PostMapping()
     public ResponseEntity post(@RequestBody Coin coin){
--- appCrypto/src/main/java/com/lucspb/appCrypto/repository/CoinRepository.java
+++ appCrypto/src/main/java/com/lucspb/appCrypto/repository/CoinRepository.java
@@ -3,13 +3,21 @@ package com.lucspb.appCrypto.repository;
 import com.lucspb.appCrypto.entity.Coin;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
 
 @Repository
 @EnableAutoConfiguration
 public class CoinRepository {
 
     private static String INSERT = "insert into coin (name, price, qtd, datetime) values (?,?,?,?)";
+
+    private static String SELECT_ALL = "select name, sum(qtd) as qtd from coin group by name";
+
     private JdbcTemplate jdbcTemplate;
 
     public CoinRepository(JdbcTemplate jdbcTemplate) {
@@ -25,5 +33,17 @@ public class CoinRepository {
         };
         jdbcTemplate.update(INSERT, attr);
         return coin;
+    }
+
+    public List<Coin> getAll(){
+        return jdbcTemplate.query(SELECT_ALL, new RowMapper<Coin>() {
+            @Override
+            public Coin mapRow(ResultSet rs, int rowNum) throws SQLException {
+                Coin coin = new Coin();
+                coin.setName(rs.getString("name"));
+                coin.setQtd(rs.getBigDecimal("qtd"));
+                return coin;
+            }
+        });
     }
 }
