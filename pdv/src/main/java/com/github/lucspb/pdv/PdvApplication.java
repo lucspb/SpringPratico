4e536e5dd14f74edebee70c3d530c2a1f5d3e2c7
Lucas Estanislau <lucasestanislau@hotmail.com>
2023-09-20 18:46:07 -0300

    iniciando um novo proj, utilizando lombok

---
+++ pdv/src/main/java/com/github/lucspb/pdv/PdvApplication.java
@@ -0,0 +1,16 @@
+package com.github.lucspb.pdv;
+
+import com.github.lucspb.model.User;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdvApplication.class, args);
+
+		User user1 = new User();
+	}
+
+}
