22c9b763d12f97b22af5cd58e6af59dfc06bdc76
Lucas Estanislau <lucasestanislau@hotmail.com>
2023-09-23 11:19:02 -0300

    criando os repositories

---
+++ pdv/src/main/java/com/github/lucspb/repository/ItemSaleRepository.java
@@ -0,0 +1,9 @@
+package com.github.lucspb.repository;
+
+import com.github.lucspb.model.ItemSale;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemSaleRepository extends JpaRepository<ItemSale, Long> {
+}
---
+++ pdv/src/main/java/com/github/lucspb/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.github.lucspb.repository;
+
+import com.github.lucspb.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
