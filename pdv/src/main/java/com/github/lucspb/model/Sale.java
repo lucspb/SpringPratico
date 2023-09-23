a19c91422a442c32d039f81bad14ff4eacb734b4
Lucas Estanislau <lucasestanislau@hotmail.com>
2023-09-23 10:59:47 -0300

    implementando relacionamento de usuario e venda

--- pdv/src/main/java/com/github/lucspb/model/Sale.java
+++ pdv/src/main/java/com/github/lucspb/model/Sale.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -18,12 +19,13 @@ public class Sale {
     private Long id;
 
     @NotNull
-    @Column(name= "sale_date")
+    @Column(name = "sale_date")
     private LocalDate saleDate;
 
     @ManyToOne
     @JoinColumn(name = "user_id")
     private User user;
 
-
+    @OneToMany(mappedBy = "sale", fetch = FetchType.LAZY)
+    private List<ItemSale> itens;
 }
--- pdv/src/main/java/com/github/lucspb/model/User.java
+++ pdv/src/main/java/com/github/lucspb/model/User.java
@@ -5,6 +5,8 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
+import java.util.List;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -20,6 +22,9 @@ public class User {
     private String name;
 
     private boolean isEnabled;
+
+    @OneToMany(mappedBy = "user")
+    private List<Sale> sales;
 
 
 }
